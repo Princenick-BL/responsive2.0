ba50e176f05e2264d30c930cdbea4468a99b1420
Nick <princenickballo@gmail.com>
2021-03-03 02:19:22 +0100

    init

---
+++ src/APP/App.js
@@ -0,0 +1,13 @@
+import  React from 'react'
+import './App.css'
+
+import MENU from '../COMPONENTS/Menu/Menu'
+function App() {
+  return (
+    <div className="App">
+        <MENU></MENU>
+    </div>
+  );
+}
+
+export default App;
