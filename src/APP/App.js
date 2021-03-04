1d01be76e51d360d844ef538c4b271a40a5ac01f
Nick <princenickballo@gmail.com>
2021-03-04 23:39:20 +0100

    v2

--- src/APP/App.js
+++ src/APP/App.js
@@ -1,7 +1,7 @@
 import  React from 'react'
 import './App.css'
 
-import MENU from '../COMPONENTS/Menu/Menu'
+import MENU from '../COMPONENTS/menu/Menu'
 function App() {
   return (
     <div className="App">
---
+++ src/COMPONENTS/menu/Menu.js
@@ -0,0 +1,85 @@
+import React,{Fragment, useState,useEffect} from 'react'
+import logo from '../../MEDIAS/eilco.png'
+import './Menu.css'
+
+export default function Menu() {
+
+    //verifier la largeur de la vue
+    const [largeur,setLargeur]=useState(window.innerWidth);
+    const [toggleMenu,setToggleMenu]=useState(false);
+
+    const showMenu =() =>{
+        setToggleMenu(!toggleMenu);
+    };
+
+    useEffect(()=>{
+
+        const changeWidth =()=>{
+
+            setLargeur(window.innerWidth);
+
+            if(window.innerWidth > 500){
+                setToggleMenu(false);
+            }
+        }
+        window.addEventListener('resize',changeWidth);
+        return()=>{
+            window.removeEventListner('resize',changeWidth);
+        }
+    },[])
+
+    return (
+
+            <Fragment>
+                <nav className="menuTop">
+                    <div className="toggleMenu flexV center" onClick={showMenu}>
+                        
+                        <div className={!toggleMenu ? "dimiss " :"flexH t"}>
+                            <div className="nav-btn-span l "></div>
+                            <div className="nav-btn-span l "></div>
+                            <div className="nav-btn-span l"></div>
+                        </div>   
+                        <div className={!toggleMenu ? "dimiss " :"flexH t"}>
+                            <div className="nav-btn-span l"></div>
+                            <div className="nav-btn-span l "></div>
+                            <div className="nav-btn-span l"></div>
+                        </div>
+                        <div className={!toggleMenu ? "dimiss " :"flexH t"}>
+                            <div className="nav-btn-span l"></div>
+                            <div className="nav-btn-span l "></div>
+                            <div className="nav-btn-span l"></div>
+                        </div>
+
+                    </div>
+  
+                </nav>
+                
+                <nav className="menu flexH" >
+                    {(largeur>500 || !toggleMenu)&&(
+                        <div className="menuContainer flexV">
+                        <img className="logo" src={logo} alt='Logo'></img>
+                        <div className="user">
+                                
+                            <div className="userFirstname">BALLO</div>
+                            <div className="userName">Prince Nick</div>
+
+                        </div>
+                        <ul className="menuOptions">
+                            <li className="MenuOption">Option 1</li>
+                            <li className="MenuOption">Option 2</li>
+                            <li className="MenuOption">Option 3</li>
+                            <li className="MenuOption">Option 4</li>                            
+                        </ul>
+                    </div>
+                     )}
+                    <div className="toggleMenu flexV" onClick={showMenu}>
+                            
+                        <div className={!toggleMenu ?"change": "dimiss " }></div>
+                        <div className={!toggleMenu ?"change": "dimiss " }></div>
+                        <div className={!toggleMenu ?"change": "dimiss " }></div>
+    
+                    </div>       
+                </nav>
+            </Fragment>
+    )
+}
